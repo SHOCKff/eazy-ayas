d82ed15360689cd51f8dba694a107b4658666a58
SHOCKff <156565597+SHOCKff@users.noreply.github.com>
2024-01-29 22:56:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-
+let c1=[-1,-3];
+let j=2;
+newcall=(ids)=>{ 
+}
 uncall=(p,ids,s1)=>{
   p.setAttribute("id",ids);
+  s1.currentTime=0;
   s1.pause();
   s1.currentTime=0;
   index--;
-  console.log("uncalled")
-  console.log(index);
+ // console.log(index);
+ console.log("uncalled");
 }
 index=0
 call=(id)=>{       //uncalling
- //b=0
   let p=document.getElementById(id);
   let s=p.getAttribute("value");
   p.setAttribute("id","clicked");
@@ -19,16 +22,20 @@ call=(id)=>{       //uncalling
   s1.play();
   index++;
   if(index>1){
-            uncall(x1,x2,x3);
-              x1=p;
-              x2=ids;
-              x3=s1;
+           // uncall(store[0],store[1],store[2]);
+           // store=[p,ids,s1]
+           uncall(x1,x2,x3);
+            x1=p;
+            x2=ids;
+            x3=s1;
+            console.log("not else")
           }
   else{
-    x1=p;
-    x2=ids;
-    x3=s1;
- }
+   // store=[p,ids,s1]
+   x1=p;
+   x2=ids;
+   x3=s1;
+   }
 
 }
 let audio=document.getElementsByClassName("audi");
@@ -43,14 +50,15 @@ for(let i=0;i<array.length;i++){
 };
 ayahsarray.forEach(element => {
    element.addEventListener("click",()=>{
-     let id=element.getAttribute("id");
-     call(id);
+     let id=element.getAttribute("id"); 
+     c1.push(parseInt(element.getAttribute("value"),10))
+     i=j-1;
+     console.log(c1)
+     if(c1[j]!=c1[i])
+     {call(id);}
+    else
+    {newcall(id);}
+    j=j+1;
    }
    )
 });
-
-
-
-
-
-
